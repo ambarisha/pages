a4f95577ae1ec53871e7a0acc9ac855c86ca9484
Ambarisha B <b.ambarisha@gmail.com>
2013-07-25 13:29:30 +0530

    First commit of "Pages" Templates already set up: - base.html - index.html - article.html - page.html

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from os import path
+
+AUTHOR = u'Ambarisha'
+SITENAME = u'Ambarisha'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+DEFAULT_PAGINATION = False
+
+THEME = path.join(path.dirname(__file__), 'hack-theme')
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
